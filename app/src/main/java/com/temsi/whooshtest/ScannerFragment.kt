2269e895b5261ab6c4b4341e853c6aa2ff149dde
artempatrin <patrik2207art@gmail.com>
2020-12-13 02:50:59 +0300

    Initial commit

---
+++ app/src/main/java/com/temsi/whooshtest/ScannerFragment.kt
@@ -0,0 +1,20 @@
+package com.temsi.whooshtest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ScannerFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scanner, container, false)
+    }
+
+}
